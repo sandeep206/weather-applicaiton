4883714c63a7fa55e9d4e8cb5c769ef7838a4fc4
Sandeep Sasidharan <sandeepsasidharan206@gmail.com>
2021-04-21 15:36:32 +0200

    - refactoring and cleanup.

--- src/app/components/weather-forecast/weather-forecast.component.ts
+++ src/app/components/weather-forecast/weather-forecast.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { Hourly } from './../../weather';
 
 @Component({
 	selector: 'weather-forecast',
 	templateUrl: './weather-forecast.component.html',
-	styleUrls: ['./weather-forecast.component.scss']
+	styleUrls: ['./weather-forecast.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class WeatherForecastComponent {
 	@Input() future: Hourly[] = [];
