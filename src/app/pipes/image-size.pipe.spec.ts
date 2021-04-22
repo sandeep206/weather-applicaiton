baf926ebacd234c1d0f2b102b0479c2ce5d3bb83
Sandeep Sasidharan <sandeepsasidharan206@gmail.com>
2021-04-22 02:53:23 +0200

    - unit test of pipe.

--- src/app/pipes/image-size.pipe.spec.ts
+++ src/app/pipes/image-size.pipe.spec.ts
@@ -1,9 +1,29 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 import { ImageSizePipe } from './image-size.pipe';
 
-describe('ImageSizePipe', () => {
+fdescribe('ImageSizePipe', () => {
+	let pipe: ImageSizePipe;
+	let icon = '';
+
+	beforeEach(() => {
+		pipe = new ImageSizePipe();
+		icon = 'icon';
+	});
+
 	it('create an instance', () => {
-		const pipe = new ImageSizePipe();
 		expect(pipe).toBeTruthy();
+	});
+
+	it('should return small icon URI', () => {
+		const size = 'small';
+		const URI = pipe.transform(icon, size);
+		const expectedURI = 'https://openweathermap.org/img/wn/icon@2x.png';
+		expect(URI).toBe(expectedURI);
+	});
+	it('should return large icon URI', () => {
+		const size = 'large';
+		const URI = pipe.transform(icon, size);
+		const expectedURI = 'https://openweathermap.org/img/wn/icon@4x.png';
+		expect(URI).toBe(expectedURI);
 	});
 });
--- src/app/pipes/whole-number.pipe.spec.ts
+++ src/app/pipes/whole-number.pipe.spec.ts
@@ -2,8 +2,18 @@
 import { WholeNumberPipe } from './whole-number.pipe';
 
 describe('WholeNumberPipe', () => {
+	let pipe: WholeNumberPipe;
+
+	beforeEach(() => {
+		pipe = new WholeNumberPipe();
+	});
+
 	it('create an instance', () => {
-		const pipe = new WholeNumberPipe();
 		expect(pipe).toBeTruthy();
+	});
+
+	it('should return a whole number', () => {
+		const output: number = pipe.transform('55.5');
+		expect(output).toBe(55);
 	});
 });
